690c44268ea39644834aab1238a117b412577d79
antoniolocascio <antonio.locascio1@gmail.com>
2025-05-27 09:59:09 +0200

    Fix

--- evm_tester/src/vm/zk_ee/mod.rs
+++ evm_tester/src/vm/zk_ee/mod.rs
@@ -145,7 +145,7 @@ impl ZKsyncOS {
             //todo: gas
             eip1559_basefee: ruint::Uint::from_str(&system_context.base_fee.to_string())
                 .expect("Invalid basefee"),
-            native_price: U256::from(1),
+            native_price: ruint::aliases::U256::from(1),
             gas_per_pubdata: Default::default(),
             block_number: system_context.block_number as u64,
             timestamp: system_context.block_timestamp as u64,
