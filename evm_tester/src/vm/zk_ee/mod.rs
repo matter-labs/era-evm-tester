b6656823c79826a8abfa1926912b238b8073ff8b
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-09-12 09:34:10 -0500

    Fmt

--- evm_tester/src/vm/zk_ee/mod.rs
+++ evm_tester/src/vm/zk_ee/mod.rs
@@ -3,10 +3,10 @@ use crate::utils::*;
 use alloy::primitives::*;
 use std::cmp::min;
 use std::str::FromStr;
-use zk_ee::utils::Bytes32;
-use zksync_os_basic_bootloader::bootloader::errors::BootloaderSubsystemError;
 use zk_ee::utils::u256_to_u64_saturated;
+use zk_ee::utils::Bytes32;
 use zksync_os_basic_bootloader::bootloader::constants::MAX_BLOCK_GAS_LIMIT;
+use zksync_os_basic_bootloader::bootloader::errors::BootloaderSubsystemError;
 use zksync_os_interface::error::InvalidTransaction;
 use zksync_os_interface::types::{BlockOutput, TxOutput};
 use zksync_os_rig::zksync_os_api::helpers;
@@ -131,10 +131,7 @@ impl ZKsyncOS {
                             zksync_os_interface::types::ExecutionOutput::Call(data) => {
                                 execution_result.return_data = data.clone();
                             }
-                            zksync_os_interface::types::ExecutionOutput::Create(
-                                data,
-                                address,
-                            ) => {
+                            zksync_os_interface::types::ExecutionOutput::Create(data, address) => {
                                 execution_result.return_data = data.clone();
                                 execution_result.address_deployed = Some(*address);
                             }
