8fd15d435b1056980248b012234449b339f6df0b
Antonio Locascio <antonio.locascio1@gmail.com>
2025-10-10 14:48:12 +0200

    fix: adapt to new metadata path (#81)

--- evm_tester/src/vm/zk_ee/mod.rs
+++ evm_tester/src/vm/zk_ee/mod.rs
@@ -8,7 +8,7 @@ use crate::utils::*;
 use alloy::primitives::*;
 use itertools::Itertools;
 use zk_ee::common_structs::derive_flat_storage_key;
-use zk_ee::system::metadata::BlockHashes;
+use zk_ee::system::metadata::zk_metadata::BlockHashes;
 use zk_ee::system::tracer::NopTracer;
 use zk_ee::utils::Bytes32;
 use zksync_os_basic_bootloader::bootloader::config::BasicBootloaderCallSimulationConfig;
